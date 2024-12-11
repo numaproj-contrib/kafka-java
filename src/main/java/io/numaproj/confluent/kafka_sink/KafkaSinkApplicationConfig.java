98b5d4c3e41cca5531497c1180c7da8872ae127d
Keran Yang <yangkr920208@gmail.com>
2024-12-11 17:33:08 -0500

    update user configration path
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- src/main/java/io/numaproj/confluent/kafka_sink/KafkaSinkApplicationConfig.java
+++ src/main/java/io/numaproj/confluent/kafka_sink/KafkaSinkApplicationConfig.java
@@ -1,18 +1,17 @@
 package io.numaproj.confluent.kafka_sink;
 
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
-import io.numaproj.confluent.kafka_sink.config.KafkaSinkerConfig;
 import io.numaproj.confluent.kafka_sink.sinker.KafkaSinker;
 import io.numaproj.numaflow.sinker.Server;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.apache.avro.generic.GenericRecord;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -23,16 +22,13 @@ import java.util.Properties;
 @ComponentScan(basePackages = "io.numaproj.confluent.kafka_sink")
 public class KafkaSinkApplicationConfig {
 
-	private final Environment environment;
+    @Value("${spring.config.location:NA}")
+    private String producerConfigFilePath;
 
-	public KafkaSinkApplicationConfig(Environment environment) {
-		this.environment = environment;
-	}
-
-	@Bean
-	public Server sinkServer(KafkaSinker kafkaSinker) {
-		return new Server(kafkaSinker);
-	}
+    @Bean
+    public Server sinkServer(KafkaSinker kafkaSinker) {
+        return new Server(kafkaSinker);
+    }
 
 	/*
 	@Bean
@@ -41,16 +37,18 @@ public class KafkaSinkApplicationConfig {
 	}
 	 */
 
-	@Bean
-	public KafkaProducer<String, GenericRecord> kafkaProducer() throws IOException {
-		Properties props = new Properties();
-		ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-		InputStream is = classloader.getResourceAsStream("kafka-producer.config");
-		props.load(is);
-		props.put(ProducerConfig.ACKS_CONFIG, "all");
-		props.put(ProducerConfig.RETRIES_CONFIG, 0);
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-		return new KafkaProducer<>(props);
-	}
+    @Bean
+    public KafkaProducer<String, GenericRecord> kafkaProducer() throws IOException {
+        log.info("producerConfigFilePath: {}", this.producerConfigFilePath);
+        Properties props = new Properties();
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("kafka-producer.config");
+        // InputStream is = new FileInputStream(this.producerConfigFilePath);
+        props.load(is);
+        props.put(ProducerConfig.ACKS_CONFIG, "all");
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        return new KafkaProducer<>(props);
+    }
 }
