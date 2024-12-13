ce4b7e58da2d471f10c7cfcc7c463629afaaa502
Keran Yang <yangkr920208@gmail.com>
2024-12-13 11:32:23 -0500

    processMessage wait for all futures
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- src/main/java/io/numaproj/confluent/kafka_sink/sinker/KafkaSinker.java
+++ src/main/java/io/numaproj/confluent/kafka_sink/sinker/KafkaSinker.java
@@ -15,13 +15,17 @@ import org.apache.avro.io.Decoder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
+import java.util.concurrent.Future;
 
 @Slf4j
 @Component
@@ -52,6 +56,7 @@ public class KafkaSinker extends Sinker implements DisposableBean {
     @Override
     public ResponseList processMessages(DatumIterator datumIterator) {
         ResponseList.ResponseListBuilder responseListBuilder = ResponseList.newBuilder();
+        Map<String, Future<RecordMetadata>> inflightTasks = new HashMap<>();
         while (true) {
             Datum datum;
             try {
@@ -64,23 +69,32 @@ public class KafkaSinker extends Sinker implements DisposableBean {
             if (datum == null) {
                 break;
             }
+            log.trace("Processing message with id: {}, payload: {}", datum.getId(), new String(datum.getValue()));
+            String key = UUID.randomUUID().toString();
+            String msg = new String(datum.getValue());
+            GenericRecord avroGenericRecord;
             try {
-                String key = UUID.randomUUID().toString();
-                String msg = new String(datum.getValue());
-                // writing original message to kafka
-                log.info("Sending message to kafka: key - {}, value - {}", key, msg);
-                String jsonData = new String(datum.getValue());
-                DatumReader<GenericRecord> reader = new GenericDatumReader<>(schema);
-                Decoder decoder = DecoderFactory.get().jsonDecoder(schema, jsonData);
-                GenericRecord result = reader.read(null, decoder);
-                ProducerRecord<String, GenericRecord> record = new ProducerRecord<>(this.topicName, key, result);
-                // TODO - this is an async call, should be sync.
-                this.producer.send(record);
-                responseListBuilder.addResponse(Response.responseOK(datum.getId()));
+                avroGenericRecord = prepareRecord(msg);
+            } catch (IOException e) {
+                log.error("Failed to prepare avro generic record from JSON data: {}", msg, e);
+                responseListBuilder.addResponse(Response.responseFailure(datum.getId(), e.getMessage()));
+                continue;
+            }
+            // TODO - remove this log statement
+            log.info("Sending message to kafka: key - {}, value - {}", key, msg);
+            ProducerRecord<String, GenericRecord> record = new ProducerRecord<>(this.topicName, key, avroGenericRecord);
+            inflightTasks.put(datum.getId(), this.producer.send(record));
+        }
+        log.debug("Number of messages inflight to the topic is {}", inflightTasks.size());
+        for (Map.Entry<String, Future<RecordMetadata>> entry : inflightTasks.entrySet()) {
+            try {
+                entry.getValue().get();
+                responseListBuilder.addResponse(Response.responseOK(entry.getKey()));
+                log.trace("Successfully processed message with id: {}", entry.getKey());
             } catch (Exception e) {
-                log.error("Failed to process message - {}", e.getMessage());
+                log.error("Failed to process message with id: {}", entry.getKey(), e);
                 responseListBuilder.addResponse(Response.responseFailure(
-                        datum.getId(),
+                        entry.getKey(),
                         e.getMessage()));
             }
         }
@@ -91,6 +105,16 @@ public class KafkaSinker extends Sinker implements DisposableBean {
     public void destroy() {
         log.info("send shutdown signal");
         log.info("kafka producer closed");
+    }
+
+    private GenericRecord prepareRecord(String jsonData) throws IOException {
+        DatumReader<GenericRecord> reader = new GenericDatumReader<>(schema);
+        try {
+            Decoder decoder = DecoderFactory.get().jsonDecoder(schema, jsonData);
+            return reader.read(null, decoder);
+        } catch (IOException e) {
+            throw new IOException("Failed to prepare avro generic record from JSON data: " + jsonData, e);
+        }
     }
 
     private Schema getSchemaForTopic(String topicName) {
